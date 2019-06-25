c06eba7e21d1021fc492378a88da2c0b8dab21a0
Durval Printes <durvalprintes@gmail.com>
2019-06-25 11:00:17 -0300

    adição do crud

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+server.use(express.json());
 
-server.get('/', (req, res ) => {
-  res.send('Hello World!');
-})
+const users = ["Durval", "Vanessa", "Paulo", "Edilene"];
+//Rota raiz
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+//Rota recupera todos
+server.get("/users", (req, res) => {
+  res.json(users);
+});
+//Rota recupera usuario
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.json(users[id]);
+});
+//Rota cria usuario
+server.post("/users/:id", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  req.json("Ok");
+});
+//Rota edita usuario
+server.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  users[id] = name;
+  req.json("Ok");
+});
+
+//Rota deleta usuario
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  users.splice(id, 1);
+  req.json("Ok");
+});
 
 server.listen(3000);
