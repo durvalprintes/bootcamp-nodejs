b4e0b0382294ba60a27a56da4392d849d80558e7
Durval Printes <durvalprintes@gmail.com>
2019-06-25 12:03:15 -0300

    ajustes e adição do  Workspace Insomnia REST Client

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ const server = express();
 server.use(express.json());
 
 const users = ["Durval", "Vanessa", "Paulo", "Edilene"];
-//Rota raiz
+//Teste
 server.get("/", (req, res) => {
   res.send("Hello World!");
 });
-//Rota recupera todos
+//Recuperar usuarios
 server.get("/users", (req, res) => {
   res.json(users);
 });
-//Rota recupera usuario
+//Recuperar usuario
 server.get("/users/:id", (req, res) => {
   const { id } = req.params;
   res.json(users[id]);
 });
-//Rota cria usuario
-server.post("/users/:id", (req, res) => {
+//Adicionar usuario
+server.post("/users", (req, res) => {
   const { name } = req.body;
   users.push(name);
-  req.json("Ok");
+  res.json("Ok");
 });
-//Rota edita usuario
+//Editar usuario
 server.put("/users/:id", (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   users[id] = name;
-  req.json("Ok");
+  res.json("Ok");
 });
 
-//Rota deleta usuario
+//Excluir usuario
 server.delete("/users/:id", (req, res) => {
   const { id } = req.params;
   users.splice(id, 1);
-  req.json("Ok");
+  res.json("Ok");
 });
 
 server.listen(3000);
